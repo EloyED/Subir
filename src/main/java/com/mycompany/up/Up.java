ba296098487629415355c63dab8ef97f0c71d26e
EloyED <eloy.cuestalahera@iesmiguelherrero.com>
2024-02-22 21:56:54 +0100

    primer commit

--- src/main/java/com/mycompany/up/Up.java
+++ src/main/java/com/mycompany/up/Up.java
@@ -11,6 +11,6 @@ package com.mycompany.up;
 public class Up {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Heello World!");
     }
 }
