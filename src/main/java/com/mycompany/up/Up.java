23fc03f48c0948dd0c57e16cf58fdfafcd30f8ca
EloyED <eloy.cuestalahera@iesmiguelherrero.com>
2024-02-22 21:35:57 +0100

    primer commit

---
+++ src/main/java/com/mycompany/up/Up.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.up;
+
+/**
+ *
+ * @author Pc
+ */
+public class Up {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
